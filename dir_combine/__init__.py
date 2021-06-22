232555263f7f81caefdafa6917703917782f16f1
Usama (PSEUDO) <pseudo.usama@gmail.com>
2021-06-22 20:39:15 +0500

    exceptional handling added for combine dir

--- dir_combine/__init__.py
+++ dir_combine/__init__.py
@@ -2,23 +2,40 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
+import json
+
 from private_constants import INPUT_DIRS, OUT_DIR
 from combine import combine
-from files import get_all_files
+from files import get_all_files, write_to_file
 
 
 COPY_FILES = True                   # If false then files will be move
 
 
-
+files = []
 for input_dir in INPUT_DIRS:
-    # Getting files
-    files = get_all_files(input_dir, relative_path=True)
+    files.append({
+        'input_dir': input_dir,
+        'files': get_all_files(input_dir, relative_path=True)
+    })
 
-    # Combining
-    combine(
-        files,
-        input_dir,
-        OUT_DIR,
-        copy_files=COPY_FILES
-    )
+files_with_err = combine(
+    files,
+    OUT_DIR,
+    copy_files=COPY_FILES
+)
+
+print(f"""
+Files with error in {'copying' if COPY_FILES else 'moving'}.
+
+    Existing files : {len(files_with_err['exists'])}
+    Invalid path   : {len(files_with_err['invalid_path'])}
+    Other errors   : {len(files_with_err['other'])}
+    ----------------------
+    Total          : {len(files_with_err['exists'])+len(files_with_err['invalid_path'])+len(files_with_err['other'])}
+
+Additional information is stored to {'file.txt'}
+"""
+)
+
+write_to_file('error-log.txt', json.dumps(files_with_err, indent=2))
--- dir_combine/combine.py
+++ dir_combine/combine.py
@@ -1,12 +1,43 @@
 from files import move_file, copy_file
 
 
-def combine(files, input_dir_path, out_dir, copy_files=True):
+def combine(files, out_dir, copy_files=True):
+    total_files_with_err = {
+        'exists': [],
+        'invalid_path': [],
+        'other': []
+    }
+
+    for a in files:
+        files_with_err = move_files_from_dir(a['files'], a['input_dir'], out_dir, copy_file)
+
+        total_files_with_err['exists'] += files_with_err['exists']
+        total_files_with_err['invalid_path'] += files_with_err['invalid_path']
+        total_files_with_err['other'] += files_with_err['other']
+
+    return total_files_with_err
+
+
+def move_files_from_dir(files, input_dir, out_dir, copy_files):
+    files_with_err = {
+        'exists': [],
+        'invalid_path': [],
+        'other': []
+    }
+    
+    move_or_copy = copy_file if copy_files else move_file
+    
     for file_relative_path in files:
-        curr_path = f'{input_dir_path}/{file_relative_path}'
+        curr_path = f'{input_dir}/{file_relative_path}'
         new_path = f'{out_dir}/{file_relative_path}'
 
-        if copy_files:
-            copy_file(curr_path, new_path)
-        else:
-            move_file(curr_path, new_path)
+        err = move_or_copy(curr_path, new_path)
+
+        if err is FileExistsError:
+            files_with_err['exists'].append(curr_path)
+        elif err is FileNotFoundError:
+            files_with_err['invalid_path'].append(curr_path)
+        elif err is not None:
+            files_with_err['other'].append(curr_path)
+
+    return files_with_err
