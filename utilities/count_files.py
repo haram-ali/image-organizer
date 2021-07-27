d3f5e571763878eb4f3db9072979f09d021a523e
Usama (PSEUDO) <pseudo.usama@gmail.com>
2021-07-27 13:47:08 +0500

    nothing special

--- utilities/count_files.py
+++ utilities/count_files.py
@@ -1,7 +1,3 @@
-# import os
-# import sys
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-
 from private_constants import INPUT_DIR
 from files import getAllFiles
 
